b4fa2bee86112b34a810268caacb3ff2d2cb81b2
dione616 <54132722+dione616@users.noreply.github.com>
2021-02-03 21:36:26 +0200

    Asc and desc chains, print are done

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,18 @@ import {
   sortArray,
   findMedian,
 } from "./actions/";
+import {
+  calculateDescChain,
+  calculateAscChain,
+} from "./actions/calculate/chain";
+import {
+  printMin,
+  printMax,
+  printDescChain,
+  printAverage,
+  printMedian,
+  printAscChain,
+} from "./actions/print";
 
 const timeStart = new Date();
 console.log(timeStart);
@@ -18,15 +30,22 @@ const run = fs.readFile("src/data/10m_2021.txt", "utf-8", (error, data) => {
   const filteredNumbers = data.split("\n").filter((x) => !isNaN(+x));
   const content = filteredNumbers.map((x) => +x);
 
+  //actions
   const [min, max] = findMinMaxNumber(content);
   const average = calculateAverage(content);
+
+  const [ascChain] = calculateAscChain(content);
+  const [descChain] = calculateDescChain(content);
+
   const sorted = sortArray(content, 0, content.length - 1);
   const median = findMedian(sorted);
 
-  console.log(`MAX is : ${max}`);
-  console.log(`MIN is : ${min}`);
-  console.log(`AVERAGE is : ${average}`);
-  console.log(`MEDIAN is : ${median}`);
+  printMin(min);
+  printMax(max);
+  printAverage(average);
+  printMedian(median);
+  printAscChain(ascChain);
+  printDescChain(descChain);
 
   //calculate time
   const timeFinish = new Date();
